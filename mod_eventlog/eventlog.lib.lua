982dcd00dfd8eee7ad2d9c18873c0939a606793b
Gordin <9ordin@gmail.com>
2014-11-11 05:49:14 +0100

    finished eventlog

--- mod_eventlog/eventlog.lib.lua
+++ mod_eventlog/eventlog.lib.lua
@@ -6,22 +6,54 @@ local st = require "util.stanza";
 
 local _M = {};
 
-function _M.new(to, from, message, session_id, timestamp, log_attrs, tags)
-    local stanza = stanza.message({ to = to, type = "normal", ["xml:lang"] = "en" })
+function _M.new(to, from, message, log_attrs, tags)
+    local stanza = stanza.message({ to = to, from = from, type = "normal", ["xml:lang"] = "en" })
 
-    local log = { xmlns = "urn:xmpp:eventlog", id = session_id }
+    local log = { xmlns = "urn:xmpp:eventlog"}
 
     if log_attrs then
         for k, v in pairs(log_attrs) do
-            log[k] = v
+            log.attr[k] = v
         end
     end
 
     local log_element = stanza:tag("log", log):tag("message"):text(message):up()
 
     if tags then
-        for tag in tags do
-            log_element:tag("tag", tag)
+        for _, tag in ipairs(tags) do
+            log_element:tag("tag", tag):up()
+        end
+    end
+
+    return stanza
+end
+
+function _M.new_log(message, log_attrs, tags)
+    local log = stanza.stanza("log", { xmlns = "urn:xmpp:eventlog"})
+
+    if log_attrs then
+        for k, v in pairs(log_attrs) do
+            log.attr[k] = v
+        end
+    end
+
+    local log_element = log:tag("message"):text(message):up()
+
+    if tags then
+        for _, tag in ipairs(tags) do
+            log_element:tag("tag", tag):up()
+        end
+    end
+
+    return log_element
+end
+
+function _M.new_from_logs(to, from, logs)
+    local stanza = stanza.message({ to = to, from = from, type = "normal", ["xml:lang"] = "en" })
+
+    if logs then
+        for _, log in ipairs(logs) do
+            stanza:add_direct_child(log)
         end
     end
 
